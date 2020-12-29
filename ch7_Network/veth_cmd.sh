b00d256eb1434b08ed38257936e29f345ccae0b8
daniel <67772752+danieltwlin@users.noreply.github.com>
2020-12-29 14:00:18 +0800

    Update veth_cmd.sh

--- ch7_Network/veth_cmd.sh
+++ ch7_Network/veth_cmd.sh
@@ -3,5 +3,11 @@
 # 查詢網絡命名空間 netns
 ip link show
 
-# 查詢 veth 設備的對端
+# 查詢 veth 設備的對端的序列號
 ip etns exec  netns1 ethtool -S veth1
+
+# 查詢 veth 設備的對端在另一個命名空間的對端
+ip netns exec  netns2 ip link | grep 5  #<--我們只關注序列號為5的設備
+
+# 會輸出類列下列的訊息，得到 veth0 為在另一個命名空間對應的端點
+# 5: veth0@if14: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc noqueue state UP mode DEFAULT group default qlen 1000
